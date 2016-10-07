b21352f7637ccea357356dba4c42d2953417eb60
Bob Watson <rbwatson@uw.edu>
2016-10-07 14:25:03 -0400

    added get_error-message function

--- rest/db_utils.php
+++ rest/db_utils.php
@@ -22,5 +22,25 @@ if (!defined('DB_UTILS')) {
 		$queryString = 'INSERT INTO '.$tableName.' ('.$dbColList.') VALUES ('.$dbValList.')';
 		return $queryString;
 	}
+	
+	function get_error_message ($link, $http_errno) {
+		// $link is provided to support future message access from database;
+		//  for now, the messages are hard-coded into this file.
+		switch ($http_errno) {
+			case 400: 
+				$error_message_text = "Bad HTTP request (400)";
+				break;
+				
+			case 404:
+				$error_message_text = "Resource not found (404)";
+				break;
+				
+			default:
+				$error_message_text = "Unrecognized error: ".$http_errno;
+				break;
+		}
+		return $error_message_text;
+	}
+	
 }
 ?>
